9e4f409372e77e7e3781a0d420042f0e93b8b448
Viqui Alaro <viquialaro241@gmail.com>
2022-02-07 19:32:31 -0400

    creacion de imagnes y asi como tambien se dio inicio al armado del titulo

--- lib/screens/basic_design.dart
+++ lib/screens/basic_design.dart
@@ -6,9 +6,38 @@ class BasicDesignScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Image(image: AssetImage('assets/landscape.jpg'))
-     ),
+    
+      body: Column(
+        children: [
+          Image(image: AssetImage('assets/landscape.jpg')),
+          
+          //Titulo
+          Title()
+        ],
+      ),
    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Column(
+          children: [
+            Text('Osheinin Lokae Camposro', style: TextStyle(fontWeight: FontWeight.bold),),
+            Text('Kardesten, Switerman',style: TextStyle(color: Colors.black45)),
+          ],
+        ),
+       
+        Icon(Icons.star,color: Colors.red,),
+        Text('41')
+      ],
+    );
   }
 }
