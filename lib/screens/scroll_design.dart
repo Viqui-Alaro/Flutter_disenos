b501726b91f4f2c84dd0e28230b97bca5f8787a8
Viqui Alaro <viquialaro241@gmail.com>
2022-02-10 22:47:11 -0400

    implementacion de texto y diseño de scroll design parte dos

--- lib/screens/scroll_design.dart
+++ lib/screens/scroll_design.dart
@@ -22,14 +22,19 @@ class MainContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final textStyle = TextStyle(fontSize: 50, fontWeight: FontWeight.bold, color: Colors.white);
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Text('11°', style: textStyle),
-        Text('Miércoles',style: textStyle),
-        Icon(Icons.keyboard_arrow_down, size: 100,color: Colors.white,)
-      ],
+    return SafeArea(
+      bottom: false,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(height: 30,),
+          Text('11°', style: textStyle),
+          Text('Miércoles',style: textStyle),
+          Expanded(child: Container()),
+          Icon(Icons.keyboard_arrow_down, size: 100,color: Colors.white,)
+        ],
+      ),
     );
   }
 }
