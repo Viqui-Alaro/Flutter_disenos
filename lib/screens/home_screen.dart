d9343a89b02c358c3efd0650f70c8978a08654f7
Viqui Alaro <viquialaro241@gmail.com>
2022-02-13 20:42:22 -0400

    Implementacion de fondo segun la maqueta planteada para menu

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_disenos/widgets/background.dart';
+
 
 class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
-     ),
+      body: Stack(
+        children: [
+          Background()
+        ],
+      )
    );
   }
 }
