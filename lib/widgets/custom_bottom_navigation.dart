4b453c0ef82a088fb37e4b2b5d3f7446cbf346a6
Viqui Alaro <viquialaro241@gmail.com>
2022-02-15 23:35:30 -0400

    Implementacion de custom bottom navigation

---
+++ lib/widgets/custom_bottom_navigation.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNavigation extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+    
+  }
+}
