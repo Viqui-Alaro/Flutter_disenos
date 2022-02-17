a572f6a99b91689a20d1f17c237bd6ea1018f2e2
Viqui Alaro <viquialaro241@gmail.com>
2022-02-16 23:08:02 -0400

    Implementacion de card primer parte

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_disenos/widgets/background.dart';
+import 'package:flutter_disenos/widgets/card_table.dart';
 import 'package:flutter_disenos/widgets/custom_bottom_navigation.dart';
 import 'package:flutter_disenos/widgets/page_title.dart';
 
@@ -30,7 +31,10 @@ class _HomeBody extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: [
-          PageTitle()
+          //titulos
+          PageTitle(),
+          //Card table
+          CardTable()
         ],
       ),
     );
---
+++ lib/widgets/card_table.dart
@@ -0,0 +1,15 @@
+
+import 'package:flutter/material.dart';
+
+
+class CardTable extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      width: 100,
+      height: 100,
+    );
+  }
+}
