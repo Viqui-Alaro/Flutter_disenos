404a5563caf94a1af0f0dc4942c401810fed4a44
Viqui Alaro <viquialaro241@gmail.com>
2022-02-16 23:18:44 -0400

    Implementacion de card segunda parte se dio forma a las tarjetas

--- lib/widgets/card_table.dart
+++ lib/widgets/card_table.dart
@@ -6,10 +6,42 @@ class CardTable extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return Table(
+      children: [
+        TableRow(
+          children: [
+            _SingleCard(),
+            _SingleCard()
+            ]
+        )
+      ],
+    );
+  }
+}
+
+
+class _SingleCard extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
-      width: 100,
-      height: 100,
+      margin: EdgeInsets.all(15),
+      height: 180,
+      decoration: BoxDecoration(
+        color: Color.fromRGBO(62, 66, 107, 0.7),
+        borderRadius: BorderRadius.circular(20)
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.blue,
+            child: Icon(Icons.pie_chart_outline_sharp,size: 35,),
+            radius: 30,
+          ),
+          Text('General',style: TextStyle(color: Colors.blue,fontSize: 18))
+        ],
+      ),
     );
   }
 }
